751e99ab758eb339f17b563de8a53e718d391b84
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-02-27 21:23:23 -0800

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,13 @@ app = flask.Flask(__name__)
 
 # TODO: create a route for the delay endpoint.
 #      The route should accept POST requests and return the same message that was sent to it.
+@app.route('/delay', methods = ['POST'])
 
+def delay():
+
+    message = flask.request.data
+
+    return(message)
 
 if __name__ == "__main__":
     app.run()
